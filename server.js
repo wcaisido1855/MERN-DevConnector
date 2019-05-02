96f1700310ba209c50f639656af8324d24f3f1ba
wcaisido1855 <wcaisido1855@gmail.com>
2019-05-01 23:45:05 -0700

    Commit 1.15 - 5th Commit. Completed Video 10, on to Video 11 now

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+//I Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
